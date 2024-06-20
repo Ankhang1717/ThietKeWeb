ac35fe14d5e0ff8c833505d83b7dd41a4315b058
Khang Le <phuleankhang@gmail.com>
2024-06-20 23:12:48 +0700

    update

--- js/total-product.js
+++ js/total-product.js
@@ -5,10 +5,12 @@ document.addEventListener("DOMContentLoaded", function() {
     // Lấy thẻ span để hiển thị tổng số sản phẩm
     var totalProductsSpan = document.getElementById('total-products-span');
     var totalProductsSpanAll = document.getElementById('total-products-span-all');
+    var totalProductsSpanAll1 = document.getElementById('total-products-span-all-1');
     // Đếm số lượng sản phẩm
     var totalProducts = products.length;
 
     // Hiển thị tổng số sản phẩm trong thẻ span
     totalProductsSpan.textContent = ' (' + totalProducts + ')';
     totalProductsSpanAll.textContent = ' (' + totalProducts + ')';
+    totalProductsSpanAll1.textContent = ' (' + totalProducts + ')';
 });
